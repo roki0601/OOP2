39ffdfe397e9621c3972bf0ba20a9bb1af816e27
Шамиль <shamilsatlykovvv@gmail.com>
2023-02-11 09:27:51 +0500

    Комментарии

--- oop2/Source.cpp
+++ oop2/Source.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <conio.h>
 
 using namespace std;
 
@@ -11,7 +10,7 @@ public:
 	{
 		cout << "Car()\n";
 		model = 1;
-		count = 1;
+		count = 1; 
 	}
 
 	Car(int model, int count)
@@ -31,7 +30,7 @@ public:
 
 	void add(int add_count)
 	{
-		count += add_count;	
+		count += add_count;
 	}
 
 	void print();
@@ -39,9 +38,7 @@ public:
 	~Car()
 	{
 		cout << "Destruction Car\t" << model << "\t" << count << "\n";
-
 	}
-
 };
 
 
@@ -104,7 +101,6 @@ void Car::print()
 
 int main()
 {
-	
 	setlocale(LC_ALL, "Russian");
 	int choice;
 menu:
@@ -117,16 +113,16 @@ menu:
 			system("cls");
 				Car c1;
 				Car c2(3, 15);
-				Car c3(c2);
+				Car c3(c2);  //обычное создание (статическое)
 				goto menu;
 				break;
 		}
 		case 2:
 		{
 			system("cls");
-			Car *c1 = new Car();
-			Car *c2= new Car(3, 15);
-			Car *c3 = new Car(*c2);
+			Car *c1 = new Car(); //создание через указатель
+			Car *c2= new Car(3, 15); //создается объект класса Car и в переменную заносится ссылка на нее
+			Car *c3 = new Car(*c2); 
 			
 			delete c1;
 			delete c2;
@@ -139,21 +135,21 @@ menu:
 			system("cls");
 			Car c1(0, 0);
 			c1.print();
-			c1.add(10);
+			c1.add(10); //демонстрация методов
 			goto menu;
 			break;
 		}
 		case 4:
 		{
 			system("cls");
-			UniqueCar uc1(1, 1, 4);
+			UniqueCar uc1(1, 1, 4); //демонстрация наследования, вызова конструкторов и деструкторов
 			goto menu;
 			break;
 		}
 		case 5:
 		{
 			system("cls");
-			Car* c = new UniqueCar(2, 2, 4);
+			Car* c = new UniqueCar(2, 2, 4); //композиция объектов 
 			delete c;
 			goto menu;
 			break;
